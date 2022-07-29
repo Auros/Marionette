1f0a5b9961acb4328852c9bb875d2a5e5cb7fc8b
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-07-29 13:07:23 -0400

    Setup logging

--- Marionette.Daemon/Program.cs
+++ Marionette.Daemon/Program.cs
@@ -1,5 +1,30 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
 
-var host = Host.CreateDefaultBuilder(args);
+HostBuilder builder = new();
 
-await host.RunConsoleAsync();
+var logger = new LoggerConfiguration()
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3} {SourceContext}] {Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
+Log.Logger = logger;
+
+SerilogLoggerFactory logFactory = new(Log.Logger);
+var programLogger = logFactory.CreateLogger<Program>();
+
+builder.ConfigureLogging((ctx, logging) =>
+{
+    logging
+        .ClearProviders()
+        .AddSerilog()
+    ;
+});
+
+builder.ConfigureServices(services =>
+{
+
+});
+
+await builder.RunConsoleAsync();
